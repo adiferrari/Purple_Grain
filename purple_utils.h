8e256b385d0bf2e88a0da7b896c2febef65485ca
5tr0bl <michael.strobl@neumann.com>
2021-09-22 18:51:05 +0200

    utils header

---
+++ purple_utils.h
@@ -0,0 +1,14 @@
+//
+//  purple_utils.h
+//  pd_granular_synth~
+//
+//  Created by Micha Strobl on 22.09.21.
+//  Copyright © 2021 Intrinsic Audio. All rights reserved.
+//
+
+#ifndef purple_utils_h
+#define purple_utils_h
+
+int get_samples_from_ms(int ms, float sr);
+
+#endif /* purple_utils_h */
