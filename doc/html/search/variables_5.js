a1bee019368a0c2ce23c4f92fea7d4dea4fd743d
Nikita Kretschmar <nikitakretschmar@yahoo.de>
2021-09-30 22:12:15 +0200

    finalofinal

--- doc/html/search/variables_5.js
+++ doc/html/search/variables_5.js
@@ -1,10 +1,10 @@
 var searchData=
 [
-  ['gauss_5fq_5ffactor_156',['gauss_q_factor',['../structc__granular__synth.html#aa164a91c3ebd564ef440719225a15f58',1,'c_granular_synth::gauss_q_factor()'],['../structpd__granular__synth__tilde.html#a1e83151d2a2da97bca1a65b3fb9ca165',1,'pd_granular_synth_tilde::gauss_q_factor()']]],
-  ['grain_5factive_157',['grain_active',['../structgrain.html#a55cdc993050e6e886756b0e338dfb85d',1,'grain']]],
-  ['grain_5findex_158',['grain_index',['../structgrain.html#aa279b25631b91e8d1dee9edd79453ff4',1,'grain']]],
-  ['grain_5fsize_159',['grain_size',['../structpd__granular__synth__tilde.html#a62f55889465ff31e02f50697bf1433a7',1,'pd_granular_synth_tilde']]],
-  ['grain_5fsize_5fms_160',['grain_size_ms',['../structc__granular__synth.html#a44f20994496035490c7aa24f92147606',1,'c_granular_synth']]],
-  ['grain_5fsize_5fsamples_161',['grain_size_samples',['../structc__granular__synth.html#abd1056818e99cc3c63d27a14010b90e4',1,'c_granular_synth::grain_size_samples()'],['../structgrain.html#ac3f579526e827c8e44bbe1e725ec9dc4',1,'grain::grain_size_samples()']]],
-  ['grains_5ftable_162',['grains_table',['../structc__granular__synth.html#a8e8e0ead288803885bda3b639cc7ef9b',1,'c_granular_synth']]]
+  ['gauss_5fq_5ffactor_157',['gauss_q_factor',['../structc__granular__synth.html#aa164a91c3ebd564ef440719225a15f58',1,'c_granular_synth::gauss_q_factor()'],['../structpd__granular__synth__tilde.html#a1e83151d2a2da97bca1a65b3fb9ca165',1,'pd_granular_synth_tilde::gauss_q_factor()']]],
+  ['grain_5factive_158',['grain_active',['../structgrain.html#a55cdc993050e6e886756b0e338dfb85d',1,'grain']]],
+  ['grain_5findex_159',['grain_index',['../structgrain.html#aa279b25631b91e8d1dee9edd79453ff4',1,'grain']]],
+  ['grain_5fsize_160',['grain_size',['../structpd__granular__synth__tilde.html#a62f55889465ff31e02f50697bf1433a7',1,'pd_granular_synth_tilde']]],
+  ['grain_5fsize_5fms_161',['grain_size_ms',['../structc__granular__synth.html#a44f20994496035490c7aa24f92147606',1,'c_granular_synth']]],
+  ['grain_5fsize_5fsamples_162',['grain_size_samples',['../structc__granular__synth.html#abd1056818e99cc3c63d27a14010b90e4',1,'c_granular_synth::grain_size_samples()'],['../structgrain.html#ac3f579526e827c8e44bbe1e725ec9dc4',1,'grain::grain_size_samples()']]],
+  ['grains_5ftable_163',['grains_table',['../structc__granular__synth.html#a8e8e0ead288803885bda3b639cc7ef9b',1,'c_granular_synth']]]
 ];
